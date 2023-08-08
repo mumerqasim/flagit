72dfd46d4e7607fd6579af34e8e5fabd0ff0e510
mumerqasim <94996377+mumerqasim@users.noreply.github.com>
2023-08-08 17:16:18 -0400

    url changes

--- src/store/urls.js
+++ src/store/urls.js
@@ -1,12 +1,12 @@
 const url = {
-    login:'http://159.89.118.97/api/auth/local',
-    changePass:'http://159.89.118.97/api/change-password',
-    getClients:'http://159.89.118.97/api/clients',
-    getVendors:'http://159.89.118.97/api/vendors',
-    getCategories:'http://159.89.118.97/api/categories',
-    getStates:'http://159.89.118.97/api/states',
-    base:'http://159.89.118.97/api/',
-    admin:'http://159.89.118.97/admin'
+    login:'https://flagit.online/api/auth/local',
+    changePass:'https://flagit.online/api/change-password',
+    getClients:'https://flagit.online/api/clients',
+    getVendors:'https://flagit.online/api/vendors',
+    getCategories:'https://flagit.online/api/categories',
+    getStates:'https://flagit.online/api/states',
+    base:'https://flagit.online/api/',
+    admin:'https://flagit.online/admin'
 }
 
 export default url;
